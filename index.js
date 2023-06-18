1909d48ceec0561e43785440445bcac632ecbd0a
Matthew <matthewjaredlevin88@gmail.com>
2023-06-18 16:38:36 -0500

    Complete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+
+  function writeCards(names, gift) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${gift} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
